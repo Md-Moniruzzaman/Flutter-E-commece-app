500d54966dfc841a66f68170851abfd6238b3cdc
Md. Moniruzzaman <ammoniruzzaman@gmail.com>
2022-03-15 23:48:09 +0600

    create color constant

---
+++ lib/constant/appColors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color white = Colors.white;
+  static const Color blue = Colors.blue;
+  static const Color deepOrange = Color.fromARGB(255, 230, 86, 42);
+}
