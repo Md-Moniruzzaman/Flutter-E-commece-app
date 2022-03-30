7d2be389c102efa8cd572cf067f1ce3425829a95
Md. Moniruzzaman <ammoniruzzaman@gmail.com>
2022-03-30 23:53:54 +0600

    Favorite page

---
+++ lib/bottomNapPages/favorite_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class Favorite extends StatefulWidget {
+  @override
+  State<Favorite> createState() => _FavoriteState();
+}
+
+class _FavoriteState extends State<Favorite> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
