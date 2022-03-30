0172adbb0903ccf8fe6c69037b520ff42a04e89a
Md. Moniruzzaman <ammoniruzzaman@gmail.com>
2022-03-30 23:52:48 +0600

    Cart page

---
+++ lib/bottomNapPages/cart_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class Cart extends StatefulWidget {
+  @override
+  State<Cart> createState() => _CartState();
+}
+
+class _CartState extends State<Cart> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
