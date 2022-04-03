47f5f6846a14ed8b0e4791708ad1c2ea803ce68a
Md. Moniruzzaman <ammoniruzzaman@gmail.com>
2022-04-03 23:36:17 +0600

    Create a search bar

--- lib/bottomNapPages/home_page.dart
+++ lib/bottomNapPages/home_page.dart
@@ -6,8 +6,21 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  final TextEditingController _searchController = TextEditingController();
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: SafeArea(
+          child: Container(
+        child: Column(
+          children: [
+            TextFormField(
+              controller: _searchController,
+              decoration: InputDecoration(fillColor: Colors.red),
+            )
+          ],
+        ),
+      )),
+    );
   }
 }
